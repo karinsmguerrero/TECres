ca58c09a0584a354d56d64bb44133cfe479cbbfd
Karina Martinez <karinamguerrero98@gmail.com>
2019-09-22 14:46:10 -0600

    models

---
+++ TECres-site/src/app/models/sales-agent.model.spec.ts
@@ -0,0 +1,7 @@
+import { SalesAgent } from './sales-agent.model';
+
+describe('SalesAgent', () => {
+  it('should create an instance', () => {
+    expect(new SalesAgent()).toBeTruthy();
+  });
+});
