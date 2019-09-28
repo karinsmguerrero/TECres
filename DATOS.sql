d825f3772b5f5409ced2cf246e5c4dd23a31e439
Dario <darior1227@gmail.com>
2019-09-27 20:47:21 -0600

    Create DATOS.sql
    datos

---
+++ DATOS.sql
@@ -0,0 +1,40 @@
+INSERT INTO UBICACION (Provincia,Canton,Distrito)
+VALUES
+ ('San Jose','San Jose','Carmen'),
+ ('San Jose','San Jose','Merced'),
+ ('San Jose','San Jose','Hospital'),
+ ('San Jose','San Jose','Catedral'),
+ ('San Jose','San Jose','Zapote'),
+ ('San Jose','San Jose','San Francisco de dos Rios'),
+ ('San Jose','San Jose','La Uruca'),
+ ('San Jose','San Jose','Mata Redonda'),
+ ('San Jose','San Jose','Pavas'),
+ ('San Jose','Goicoechea','Guadalupe'),
+ ('San Jose','Goicoechea','Calle Blancos'),
+ ('San Jose','Goicoechea','Ipis'),
+ ('San Jose','Goicoechea','Rancho Redondo'),
+ ('San Jose','Goicoechea','Purral'),
+ ('Cartago','Turrialba','Peralta'),
+ ('Cartago','Turrialba','Tuis'),
+ ('Cartago','Turrialba','Tayutic'),
+ ('Cartago','Turrialba','Pavones');
+
+ INSERT INTO TIPO_PISO (Tipo)
+VALUES ('Concreto Lujado'),('Porcelanata'),('Ceramica');
+
+INSERT INTO NACIONALIDAD(Nacionalidad)
+VALUES ('Costarricense'),
+('Nicaraguense'),
+('Venezolano'),
+('Canadiense');
+
+INSERT INTO OCUPACION(Nombre)
+VALUES('Ingeniero'),
+('Medico'),('Estilista'),('Enfermero'),('Veterinario');
+
+INSERT INTO INMUEBLE(Tipo)
+VALUES('Lote'),('Casa'),('Apartemento');
+
+INSERT INTO PERFIL_CLIENTE(Descripcion)
+VALUES('Agente'),('Constructor'),('Propietario'),('Anunciante');
+
