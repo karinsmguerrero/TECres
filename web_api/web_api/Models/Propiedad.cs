2df0507b4c5cd25fa0d55b1f7714785afc589782
geraldvm <gerald-_16@hotmail.com>
2019-10-14 00:02:32 -0600

    Controladores

--- web_api/web_api/Models/Propiedad.cs
+++ web_api/web_api/Models/Propiedad.cs
@@ -23,4 +23,26 @@ namespace web_api.Models
         public string Propietario { get; set; }
         public Nullable<bool> Gimnasio { get; set; }
     }
+
+
+    public class PropiedadU
+    {
+        public int Id { get; set; }
+        public int CantidadBanos { get; set; }
+        public int Provincia { get; set; }
+        public int Canton { get; set; }
+        public int Distrito { get; set; }
+        public string TipoPropiedad { get; set; }
+        public int TamanoLote { get; set; }
+        public int TamanoPropiedad { get; set; }
+        public bool Piscina { get; set; }
+        public bool ParqueoVisitas { get; set; }
+        public bool Parqueo { get; set; }
+        public int TipoPiso { get; set; }
+        public int CantidadNiveles { get; set; }
+        public int CantidadHabitaciones { get; set; }
+        public int Precio { get; set; }
+        public string Propietario { get; set; }
+        public bool Gimnasio { get; set; }
+    }
 }
