65bc49b06fe2d4ec9ca9c8d120f5c767c921d3dd
geraldvm <gerald-_16@hotmail.com>
2019-10-17 02:01:09 -0600

    Cambios Finales

---
+++ ALTER TABLE.sql
@@ -0,0 +1,74 @@
+/*******************************************************************************************************
+********************************************************************************************************
+*******************											********************************************
+*******************											********************************************
+*******************			        TEC-RES					********************************************
+*******************											********************************************
+*******************											********************************************
+********************************************************************************************************
+********************************************************************************************************/
+
+--ALTER TABLE TECRES ASIGNACION DE LLAVES FORANEAS
+GO
+ALTER TABLE COMPRADOR
+ADD FOREIGN KEY(Username) REFERENCES USUARIO(Username)
+GO
+ALTER TABLE PROPIEDAD
+ADD FOREIGN KEY (IdUbicacion) REFERENCES UBICACION(Id); 
+GO
+ALTER TABLE PROPIEDAD
+ADD FOREIGN KEY (TipoPropiedad) REFERENCES INMUEBLE(Id); 
+GO
+ALTER TABLE PROPIEDAD
+ADD FOREIGN KEY (TipoPiso) REFERENCES TIPO_PISO(Id); 
+GO
+ALTER TABLE PROPIEDAD
+ADD FOREIGN KEY (Propietario) REFERENCES CLIENTE(Username); 
+GO
+ALTER TABLE ANUNCIO
+ADD FOREIGN KEY (IdPropiedad) REFERENCES PROPIEDAD(Id); 
+GO	
+ALTER TABLE ANUNCIO
+ADD FOREIGN KEY (TipoAnuncio) REFERENCES TIPO_ANUNCIO(Nombre); 
+GO
+ALTER TABLE ANUNCIO
+ADD FOREIGN KEY (IdAgente) REFERENCES CLIENTE(Username); 
+GO
+ALTER TABLE MENSAJE
+ADD FOREIGN KEY (IdAnuncio) REFERENCES ANUNCIO(Id); 
+GO
+ALTER TABLE MENSAJE
+ADD FOREIGN KEY (Comprador) REFERENCES COMPRADOR(Username); 
+GO
+ALTER TABLE MENSAJE
+ADD FOREIGN KEY (Agente) REFERENCES CLIENTE(Username); 
+GO
+ALTER TABLE COMPRADOR
+ADD FOREIGN KEY (IdOcupacion) REFERENCES OCUPACION(Id); 
+GO
+ALTER TABLE COMPRADOR
+ADD FOREIGN KEY (Domicilio) REFERENCES UBICACION(Id); 
+GO
+ALTER TABLE CLIENTE
+ADD FOREIGN KEY (PerfilCliente) REFERENCES PERFIL_CLIENTE(Nombre); 
+GO
+ALTER TABLE CLIENTE
+ADD FOREIGN KEY (IdNacionalidad) REFERENCES NACIONALIDAD(Id); 
+GO
+ALTER TABLE CLIENTE
+ADD FOREIGN KEY (Username) REFERENCES USUARIO(Username); 
+GO
+ALTER TABLE ADMINISTRADOR
+ADD FOREIGN KEY (Username) REFERENCES USUARIO(Username); 
+GO
+ALTER TABLE FOTOS 
+ADD FOREIGN KEY (IdPropiedad) REFERENCES PROPIEDAD(Id);
+GO
+ALTER TABLE ANUNCIO
+ADD FOREIGN KEY (IdPublico) REFERENCES PUBLICO(Id); 
+GO
+ALTER TABLE FACTURA
+ADD FOREIGN KEY (IdAnuncio) REFERENCES ANUNCIO(Id)
+--ALTER TABLE USUARIO DROP COLUMN Contrasena; 
+
+
---
+++ Create.sql
@@ -0,0 +1,187 @@
+/*******************************************************************************************************
+********************************************************************************************************
+*******************											********************************************
+*******************											********************************************
+*******************			        TEC-RES					********************************************
+*******************											********************************************
+*******************											********************************************
+********************************************************************************************************
+********************************************************************************************************/
+
+--CREATE TABLE
+
+CREATE TABLE USUARIO(
+
+	Username VARCHAR(50) NOT NULL,
+	Nombre VARCHAR(15) NOT NULL,
+	PrimerApellido VARCHAR(15) NOT NULL,
+	SegundoApellido VARCHAR(15) NOT NULL,
+	FechaIngreso DATE,
+	Contrasena VARCHAR(100) NOT NULL,
+	Correo VARCHAR(50) NOT NULL,
+	PRIMARY KEY (Username)
+	--Agregar correo
+)
+
+CREATE TABLE OCUPACION(
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(20) NOT NULL,
+	PRIMARY KEY (Id),
+	UNIQUE(Nombre)
+)
+
+
+CREATE TABLE COMPRADOR(
+
+	Username VARCHAR(50) NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	IngresoMensual INT NOT NULL,
+	IdOcupacion INT NOT NULL,
+	Domicilio INT NOT NULL,
+	Sexo VARCHAR(5) NOT NULL,
+	PRIMARY KEY (Username),
+	
+)
+
+
+
+
+
+CREATE TABLE PERFIL_CLIENTE(
+	Nombre VARCHAR(20) NOT NULL,
+	Descripcion VARCHAR(200) NOT NULL,
+	Predeterminado BIT DEFAULT (0),
+	PRIMARY KEY (Nombre)
+)
+
+CREATE TABLE CLIENTE(
+	Username VARCHAR(50) NOT NULL,
+	IdNacionalidad INT NOT NULL,
+	Cedula INT NOT NULL,
+	PerfilCliente VARCHAR(20) NOT NULL,
+	PRIMARY KEY (Username),
+	UNIQUE (Cedula)
+)
+
+
+CREATE TABLE ADMINISTRADOR(
+	Username VARCHAR(50) NOT NULL,
+	Cedula INT NOT NULL,
+	PRIMARY KEY (Username),
+	UNIQUE (Cedula)
+)
+
+
+CREATE TABLE MENSAJE(
+	Id INT NOT NULL IDENTITY(1,1),
+	IdAnuncio INT NOT NULL,
+	Fecha DATE NOT NULL,
+	Hora TIME NOT NULL,
+	Texto VARCHAR(80) NOT NULL,
+	Comprador VARCHAR(50) NOT NULL,
+	Agente VARCHAR(50) NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+CREATE TABLE ANUNCIO(
+	Id INT NOT NULL IDENTITY(1,1),
+	IdPropiedad INT NOT NULL,
+	TipoAnuncio VARCHAR(30) NOT NULL,
+	Tarjeta INT NOT NULL,
+	IdAgente VARCHAR(50) NOT NULL,
+	Estado BIT DEFAULT(0),
+	Venta BIT DEFAULT (1),
+	IdPublico INT,
+	FechaInicial DATE NOT NULL,
+	FechaFinal DATE NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+CREATE TABLE TIPO_ANUNCIO(
+	Nombre VARCHAR(30) NOT NULL,
+	Precio INT NOT NULL,
+	Predeterminado BIT DEFAULT (0),
+	PRIMARY KEY (Nombre)
+
+)
+
+CREATE TABLE PROPIEDAD(
+	Id INT NOT NULL IDENTITY(1,1),
+	Titulo VARCHAR(50) NOT NULL,
+	CantidadBanos INT NOT NULL,
+	IdUbicacion INT NOT NULL,
+	TipoPropiedad INT NOT NULL,
+	TamanoLote INT NOT NULL,
+	TamanoPropiedad INT NOT NULL,
+	Piscina BIT NOT NULL,
+	ParqueoVisitas BIT NOT NULL,
+	Parqueo BIT NOT NULL,
+	TipoPiso INT NOT NULL,
+	CantidadNiveles INT DEFAULT (1),
+	CantidadHabitaciones INT NOT NULL,
+	Precio INT NOT NULL,
+	Propietario VARCHAR(50) NOT NULL,
+	Gimnasio BIT DEFAULT(0),
+	PRIMARY KEY (Id)
+)
+
+CREATE TABLE INMUEBLE(
+	Id INT NOT NULL IDENTITY(1,1),
+	TipoPropiedad VARCHAR(30) NOT NULL, 
+	Predeterminado BIT DEFAULT (0),
+	PRIMARY KEY (Id),
+	UNIQUE(TipoPropiedad)
+)
+
+
+CREATE TABLE UBICACION(
+	Id INT NOT NULL IDENTITY(1,1),
+	Provincia VARCHAR(30) NOT NULL,
+	Canton VARCHAR(50) NOT NULL,
+	Distrito VARCHAR(50) NOT NULL,
+	PRIMARY KEY (Id)
+
+)
+
+CREATE TABLE TIPO_PISO(
+	Id INT NOT NULL IDENTITY(1,1),
+	Piso VARCHAR(30) NOT NULL,
+	Predeterminado BIT DEFAULT (0),
+	PRIMARY KEY (Id)
+
+)
+
+CREATE TABLE NACIONALIDAD(
+	Id INT NOT NULL IDENTITY(1,1),
+	NAcionalidad VARCHAR(50) NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+
+CREATE TABLE FOTOS(
+	Id INT NOT NULL IDENTITY(1,1),
+	Ruta VARCHAR(350),
+	IdPropiedad INT NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+
+CREATE TABLE PUBLICO(
+	Id INT NOT NULL IDENTITY(1,1),
+	CantidadMensajes INT NOT NULL,
+	IngresoInicial INT NOT NULL,
+	IngresoFinal INT NOT NULL,
+	Sexo VARCHAR(5) NOT NULL,
+	EdadInicial INT NOT NULL,
+	EdadFinal INT NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+
+CREATE TABLE FACTURA(
+	Id INT NOT NULL IDENTITY(1,1),
+	Monto INT NOT NULL,
+	IdAnuncio INT NOT NULL,
+	Fecha DATE,
+	PRIMARY KEY (Id)
+)
