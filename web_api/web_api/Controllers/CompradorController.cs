c13d9bda3238e110450dea9f9285ea3533aa0f21
geraldvm <gerald-_16@hotmail.com>
2019-10-17 19:47:27 -0600

    Add Controladores de mensajes

---
+++ web_api/web_api/Controllers/CompradorController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    public class CompradorController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("api/GetCompradores")]
+        public HttpResponseMessage GetClientes5()
+        {
+            using (var db = new TecEntities())
+            {
+
+                var list = db.Database.SqlQuery<CompradorU>("SELECT Username,Nombre,Sexo,Provincia,Canton," +
+                    "Distrito,FechaNacimiento,Ocupacion,IngresoMensual" +
+                    " FROM vCOMPRADORES").ToList();
+
+                Console.WriteLine(list);
+                return this.Request.CreateResponse(HttpStatusCode.OK, list);
+            }
+        }
+        
+        [HttpPost]
+        [Route("api/AddCliente")]
+        public HttpResponseMessage AddCliente([FromBody] Comprador c)
+        {
+            try
+            {
+
+
+                using (var db = new TecEntities())
+                {
+                    SqlParameter[] parameters = new SqlParameter[]
+                    {
+                        new SqlParameter("@username", c.Username),new SqlParameter("@contrasena", c.Contrasena),
+                        new SqlParameter("@nombre", c.Nombre), new SqlParameter("@pApellido", c.PrimerApellido),
+                        new SqlParameter("@sApellido", c.SegundoApellido), new SqlParameter("@fecha", c.FechaIngreso),
+                        new SqlParameter("@ocupacion", c.IdOcupacion), new SqlParameter("@cedula", c.Cedula),
+                        new SqlParameter("@perfil", c.Sexo),new SqlParameter("@correo", c.Correo),new SqlParameter("@domicilio", c.Domicilio),
+                        new SqlParameter("@nacimiento", c.FechaNacimiento),new SqlParameter("@mensualidad", c.IngresoMensual)
+
+
+                    };
+
+                    var status = db.Database.ExecuteSqlCommand("EXEC NuevoComprador @username, @contrasena," +
+                        "@correo, @nombre, @pApellido, @sApellido, @fecha, @cedula, @ocupacion," +
+                        "@mensualidad, @domicilio, @sexo, @nacimiento", parameters);
+
+                    return this.Request.CreateResponse(HttpStatusCode.OK, status);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR :" + ex);
+                return this.Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+    }
+}
--- web_api/web_api/Models/Comprador.cs
+++ web_api/web_api/Models/Comprador.cs
@@ -13,6 +13,25 @@ namespace web_api.Models
         public int IdOcupacion { get; set; }
         public int Domicilio { get; set; }
         public string Sexo { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public System.DateTime FechaIngreso { get; set; }
+        public int Cedula { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+    }
+    public class CompradorU
+    {
+        public string Username { get; set; }
+        public string Nombre { get; set; }
+        public System.DateTime FechaNacimiento { get; set; }
+        public int IngresoMensual { get; set; }
+        public string Ocupacion { get; set; }
+        public string Provincia { get; set; }
+        public string Canton { get; set; }
+        public string Distrito { get; set; }
+        public string Sexo { get; set; }
 
     }
 }
