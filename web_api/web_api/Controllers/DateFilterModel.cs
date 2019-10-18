e2e2b434d6ae23c17ac31a7363dc491a5bfb7f68
Karina Martinez Guerrero <karinamguerrero98@gmail.com>
2019-10-17 22:19:59 -0600

    actualizar app

---
+++ web_api/web_api/Controllers/DateFilterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api.Models
+{
+    public class DateFilterModel
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
